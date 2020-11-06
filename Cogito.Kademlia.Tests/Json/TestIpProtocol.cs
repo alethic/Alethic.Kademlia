8ff5cb905f7252013a728acbc73513fc6e806c25
Jerome Haltom <jhaltom@alethic.solutions>
2020-11-05 20:00:21 -0600

    Disable some tests.

--- Cogito.Kademlia.Tests/Json/TestIpProtocol.cs
+++ Cogito.Kademlia.Tests/Json/TestIpProtocol.cs
@@ -1,43 +1,43 @@
-﻿using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
+﻿//using System.Collections.Generic;
+//using System.Threading;
+//using System.Threading.Tasks;
 
-using Cogito.Kademlia.Network;
+//using Cogito.Kademlia.Network;
 
-namespace Cogito.Kademlia.Tests.Json
-{
+//namespace Cogito.Kademlia.Tests.Json
+//{
 
-    class TestIpProtocol<TNodeId> : IKIpProtocol<TNodeId>
-        where TNodeId : unmanaged
-    {
+//    class TestIpProtocol<TNodeId> : IKIpProtocol<TNodeId>
+//        where TNodeId : unmanaged
+//    {
 
-        public IEnumerable<IKProtocolEndpoint<TNodeId>> Endpoints => throw new System.NotImplementedException();
+//        public IEnumerable<IKProtocolEndpoint<TNodeId>> Endpoints => throw new System.NotImplementedException();
 
-        public IKProtocolEndpoint<TNodeId> CreateEndpoint(in KIpEndpoint endpoint)
-        {
-            return new KIpProtocolEndpoint<TNodeId>(this, endpoint);
-        }
+//        public IKProtocolEndpoint<TNodeId> CreateEndpoint(in KIpEndpoint endpoint)
+//        {
+//            return new KIpProtocolEndpoint<TNodeId>(this, endpoint);
+//        }
 
-        public ValueTask<KResponse<TNodeId, KFindNodeResponse<TNodeId>>> FindNodeAsync(IKProtocolEndpoint<TNodeId> target, in KFindNodeRequest<TNodeId> request, CancellationToken cancellationToken)
-        {
-            throw new System.NotImplementedException();
-        }
+//        public ValueTask<KResponse<TNodeId, KFindNodeResponse<TNodeId>>> FindNodeAsync(IKProtocolEndpoint<TNodeId> target, in KFindNodeRequest<TNodeId> request, CancellationToken cancellationToken)
+//        {
+//            throw new System.NotImplementedException();
+//        }
 
-        public ValueTask<KResponse<TNodeId, KFindValueResponse<TNodeId>>> FindValueAsync(IKProtocolEndpoint<TNodeId> target, in KFindValueRequest<TNodeId> request, CancellationToken cancellationToken)
-        {
-            throw new System.NotImplementedException();
-        }
+//        public ValueTask<KResponse<TNodeId, KFindValueResponse<TNodeId>>> FindValueAsync(IKProtocolEndpoint<TNodeId> target, in KFindValueRequest<TNodeId> request, CancellationToken cancellationToken)
+//        {
+//            throw new System.NotImplementedException();
+//        }
 
-        public ValueTask<KResponse<TNodeId, KPingResponse<TNodeId>>> PingAsync(IKProtocolEndpoint<TNodeId> target, in KPingRequest<TNodeId> request, CancellationToken cancellationToken)
-        {
-            throw new System.NotImplementedException();
-        }
+//        public ValueTask<KResponse<TNodeId, KPingResponse<TNodeId>>> PingAsync(IKProtocolEndpoint<TNodeId> target, in KPingRequest<TNodeId> request, CancellationToken cancellationToken)
+//        {
+//            throw new System.NotImplementedException();
+//        }
 
-        public ValueTask<KResponse<TNodeId, KStoreResponse<TNodeId>>> StoreAsync(IKProtocolEndpoint<TNodeId> target, in KStoreRequest<TNodeId> request, CancellationToken cancellationToken)
-        {
-            throw new System.NotImplementedException();
-        }
+//        public ValueTask<KResponse<TNodeId, KStoreResponse<TNodeId>>> StoreAsync(IKProtocolEndpoint<TNodeId> target, in KStoreRequest<TNodeId> request, CancellationToken cancellationToken)
+//        {
+//            throw new System.NotImplementedException();
+//        }
 
-    }
+//    }
 
-}
+//}
--- Cogito.Kademlia.Tests/KFixedRoutingTableTests.cs
+++ Cogito.Kademlia.Tests/KFixedRoutingTableTests.cs
@@ -1,210 +1,184 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading;
+//using System.Threading.Tasks;
 
-using FluentAssertions;
+//using FluentAssertions;
 
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Cogito.Kademlia.Tests
-{
+//namespace Cogito.Kademlia.Tests
+//{
 
-    [TestClass]
-    public class KFixedRoutingTableTests
-    {
+//    [TestClass]
+//    public class KFixedRoutingTableTests
+//    {
 
-        class FakeEndpointInvoker<TNodeId> : IKInvokerPolicy<TNodeId>
-            where TNodeId : unmanaged
-        {
+//        [TestMethod]
+//        public void Should_find_proper_bucket_for_int32()
+//        {
+//            var s = new KNodeId32(0);
+//            KFixedTableRouter.GetBucketIndex(s, new KNodeId32(1)).Should().Be(0);
+//            KFixedTableRouter.GetBucketIndex(s, new KNodeId32(2)).Should().Be(1);
+//            KFixedTableRouter.GetBucketIndex(s, new KNodeId32(uint.MaxValue)).Should().Be(31);
+//        }
 
-            public ValueTask<KResponse<TNodeId, KPingResponse<TNodeId>>> PingAsync(IKProtocolEndpointSet<TNodeId> endpoints, CancellationToken cancellationToken = default)
-            {
-                return new ValueTask<KResponse<TNodeId, KPingResponse<TNodeId>>>();
-            }
+//        [TestMethod]
+//        public async Task Can_randomly_populate_int32()
+//        {
+//            var s = new KNodeId32(0);
+//            var t = new KFixedTableRouter<KNodeId32>(s, new FakeEndpointInvoker<KNodeId32>());
 
-            public ValueTask<KResponse<TNodeId, KStoreResponse<TNodeId>>> StoreAsync(IKProtocolEndpointSet<TNodeId> endpoints, in TNodeId key, KStoreRequestMode mode, in KValueInfo? value, CancellationToken cancellationToken = default)
-            {
-                return new ValueTask<KResponse<TNodeId, KStoreResponse<TNodeId>>>();
-            }
+//            var r = new Random();
+//            for (int i = 0; i < 262144 * 8; i++)
+//                await t.UpdatePeerAsync(new KNodeId32((uint)r.Next(int.MinValue, int.MaxValue)), null);
+//        }
 
-            public ValueTask<KResponse<TNodeId, KFindNodeResponse<TNodeId>>> FindNodeAsync(IKProtocolEndpointSet<TNodeId> endpoints, in TNodeId key, CancellationToken cancellationToken = default)
-            {
-                return new ValueTask<KResponse<TNodeId, KFindNodeResponse<TNodeId>>>();
-            }
+//        [TestMethod]
+//        public async Task Can_randomly_populate_int32_double()
+//        {
+//            var s = new KNodeId32(0);
+//            var t = new KFixedTableRouter<KNodeId32>(s, new FakeEndpointInvoker<KNodeId32>());
 
-            public ValueTask<KResponse<TNodeId, KFindValueResponse<TNodeId>>> FindValueAsync(IKProtocolEndpointSet<TNodeId> endpoints, in TNodeId key, CancellationToken cancellationToken = default)
-            {
-                return new ValueTask<KResponse<TNodeId, KFindValueResponse<TNodeId>>>();
-            }
+//            for (int i = 1; i <= 262144 * 8; i++)
+//                await t.UpdatePeerAsync(new KNodeId32((uint)i), null);
 
-        }
+//            var c = t.Count;
 
-        [TestMethod]
-        public void Should_find_proper_bucket_for_int32()
-        {
-            var s = new KNodeId32(0);
-            KFixedTableRouter.GetBucketIndex(s, new KNodeId32(1)).Should().Be(0);
-            KFixedTableRouter.GetBucketIndex(s, new KNodeId32(2)).Should().Be(1);
-            KFixedTableRouter.GetBucketIndex(s, new KNodeId32(uint.MaxValue)).Should().Be(31);
-        }
+//            for (int i = 1; i <= 262144 * 8; i++)
+//                await t.UpdatePeerAsync(new KNodeId32((uint)i), null);
 
-        [TestMethod]
-        public async Task Can_randomly_populate_int32()
-        {
-            var s = new KNodeId32(0);
-            var t = new KFixedTableRouter<KNodeId32>(s, new FakeEndpointInvoker<KNodeId32>());
+//            t.Count.Should().Be(c);
+//        }
 
-            var r = new Random();
-            for (int i = 0; i < 262144 * 8; i++)
-                await t.UpdatePeerAsync(new KNodeId32((uint)r.Next(int.MinValue, int.MaxValue)), null);
-        }
+//        [TestMethod]
+//        public async Task Can_randomly_populate_int32_mt()
+//        {
+//            var s = new KNodeId32(0);
+//            var t = new KFixedTableRouter<KNodeId32>(s, new FakeEndpointInvoker<KNodeId32>());
 
-        [TestMethod]
-        public async Task Can_randomly_populate_int32_double()
-        {
-            var s = new KNodeId32(0);
-            var t = new KFixedTableRouter<KNodeId32>(s, new FakeEndpointInvoker<KNodeId32>());
+//            var r = new Random();
+//            var l = new List<Task>();
+//            for (int i = 0; i < 1024; i++)
+//                l.Add(t.UpdatePeerAsync(new KNodeId32((uint)r.Next(int.MinValue, int.MaxValue)), null).AsTask());
 
-            for (int i = 1; i <= 262144 * 8; i++)
-                await t.UpdatePeerAsync(new KNodeId32((uint)i), null);
+//            await Task.WhenAll(l);
+//        }
 
-            var c = t.Count;
+//        [TestMethod]
+//        public async Task Can_randomly_populate_int64()
+//        {
+//            var s = new KNodeId64(0);
+//            var t = new KFixedTableRouter<KNodeId64>(s, new FakeEndpointInvoker<KNodeId64>());
 
-            for (int i = 1; i <= 262144 * 8; i++)
-                await t.UpdatePeerAsync(new KNodeId32((uint)i), null);
+//            var r = new Random();
+//            for (int i = 0; i < 262144 * 8; i++)
+//                await t.UpdatePeerAsync(new KNodeId64((ulong)r.NextInt64()), null);
+//        }
 
-            t.Count.Should().Be(c);
-        }
+//        [TestMethod]
+//        public async Task Can_randomly_populate_int64_mt()
+//        {
+//            var s = new KNodeId64(0);
+//            var t = new KFixedTableRouter<KNodeId64>(s, new FakeEndpointInvoker<KNodeId64>());
 
-        [TestMethod]
-        public async Task Can_randomly_populate_int32_mt()
-        {
-            var s = new KNodeId32(0);
-            var t = new KFixedTableRouter<KNodeId32>(s, new FakeEndpointInvoker<KNodeId32>());
+//            var r = new Random();
+//            var l = new List<Task>();
+//            for (int i = 0; i < 1024; i++)
+//                l.Add(t.UpdatePeerAsync(new KNodeId64((ulong)r.NextInt64()), null).AsTask());
 
-            var r = new Random();
-            var l = new List<Task>();
-            for (int i = 0; i < 1024; i++)
-                l.Add(t.UpdatePeerAsync(new KNodeId32((uint)r.Next(int.MinValue, int.MaxValue)), null).AsTask());
+//            await Task.WhenAll(l);
+//        }
 
-            await Task.WhenAll(l);
-        }
+//        [TestMethod]
+//        public async Task Can_randomly_populate_int128()
+//        {
+//            var s = new KNodeId128(Guid.Empty);
+//            var t = new KFixedTableRouter<KNodeId128>(s, new FakeEndpointInvoker<KNodeId128>());
 
-        [TestMethod]
-        public async Task Can_randomly_populate_int64()
-        {
-            var s = new KNodeId64(0);
-            var t = new KFixedTableRouter<KNodeId64>(s, new FakeEndpointInvoker<KNodeId64>());
+//            for (int i = 0; i < 262144 * 8; i++)
+//                await t.UpdatePeerAsync(new KNodeId128(Guid.NewGuid()), null);
+//        }
 
-            var r = new Random();
-            for (int i = 0; i < 262144 * 8; i++)
-                await t.UpdatePeerAsync(new KNodeId64((ulong)r.NextInt64()), null);
-        }
+//        [TestMethod]
+//        public async Task Can_randomly_populate_int128_mt()
+//        {
+//            var s = new KNodeId128(Guid.Empty);
+//            var t = new KFixedTableRouter<KNodeId128>(s, new FakeEndpointInvoker<KNodeId128>());
 
-        [TestMethod]
-        public async Task Can_randomly_populate_int64_mt()
-        {
-            var s = new KNodeId64(0);
-            var t = new KFixedTableRouter<KNodeId64>(s, new FakeEndpointInvoker<KNodeId64>());
+//            var l = new List<Task>();
+//            for (int i = 0; i < 1024; i++)
+//                l.Add(t.UpdatePeerAsync(new KNodeId128(Guid.NewGuid()), null).AsTask());
 
-            var r = new Random();
-            var l = new List<Task>();
-            for (int i = 0; i < 1024; i++)
-                l.Add(t.UpdatePeerAsync(new KNodeId64((ulong)r.NextInt64()), null).AsTask());
+//            await Task.WhenAll(l);
+//        }
 
-            await Task.WhenAll(l);
-        }
+//        [TestMethod]
+//        public async Task Can_randomly_populate_int160()
+//        {
+//            var s = new KNodeId160(0, 0, 0);
+//            var t = new KFixedTableRouter<KNodeId160>(s, new FakeEndpointInvoker<KNodeId160>());
 
-        [TestMethod]
-        public async Task Can_randomly_populate_int128()
-        {
-            var s = new KNodeId128(Guid.Empty);
-            var t = new KFixedTableRouter<KNodeId128>(s, new FakeEndpointInvoker<KNodeId128>());
+//            var r = new Random();
+//            for (int i = 0; i < 262144 * 8; i++)
+//                await t.UpdatePeerAsync(new KNodeId160((ulong)r.NextInt64(), (ulong)r.NextInt64(), (uint)r.Next(int.MinValue, int.MaxValue)), null);
+//        }
 
-            for (int i = 0; i < 262144 * 8; i++)
-                await t.UpdatePeerAsync(new KNodeId128(Guid.NewGuid()), null);
-        }
+//        [TestMethod]
+//        public async Task Can_randomly_populate_int160_mt()
+//        {
+//            var s = new KNodeId160(0, 0, 0);
+//            var t = new KFixedTableRouter<KNodeId160>(s, new FakeEndpointInvoker<KNodeId160>());
 
-        [TestMethod]
-        public async Task Can_randomly_populate_int128_mt()
-        {
-            var s = new KNodeId128(Guid.Empty);
-            var t = new KFixedTableRouter<KNodeId128>(s, new FakeEndpointInvoker<KNodeId128>());
+//            var r = new Random();
+//            var l = new List<Task>();
+//            for (int i = 0; i < 1024; i++)
+//                l.Add(t.UpdatePeerAsync(new KNodeId160((ulong)r.NextInt64(), (ulong)r.NextInt64(), (uint)r.Next(int.MinValue, int.MaxValue)), null).AsTask());
 
-            var l = new List<Task>();
-            for (int i = 0; i < 1024; i++)
-                l.Add(t.UpdatePeerAsync(new KNodeId128(Guid.NewGuid()), null).AsTask());
+//            await Task.WhenAll(l);
+//        }
 
-            await Task.WhenAll(l);
-        }
+//        [TestMethod]
+//        public async Task Can_randomly_populate_int256()
+//        {
+//            var s = new KNodeId256(0, 0, 0, 0);
+//            var t = new KFixedTableRouter<KNodeId256>(s, new FakeEndpointInvoker<KNodeId256>());
 
-        [TestMethod]
-        public async Task Can_randomly_populate_int160()
-        {
-            var s = new KNodeId160(0, 0, 0);
-            var t = new KFixedTableRouter<KNodeId160>(s, new FakeEndpointInvoker<KNodeId160>());
+//            var r = new Random();
+//            for (int i = 0; i < 262144 * 8; i++)
+//                await t.UpdatePeerAsync(new KNodeId256((ulong)r.NextInt64(), (ulong)r.NextInt64(), (ulong)r.NextInt64(), (ulong)r.NextInt64()), null);
+//        }
 
-            var r = new Random();
-            for (int i = 0; i < 262144 * 8; i++)
-                await t.UpdatePeerAsync(new KNodeId160((ulong)r.NextInt64(), (ulong)r.NextInt64(), (uint)r.Next(int.MinValue, int.MaxValue)), null);
-        }
+//        [TestMethod]
+//        public async Task Can_randomly_populate_int256_mt()
+//        {
+//            var s = new KNodeId256(0, 0, 0, 0);
+//            var t = new KFixedTableRouter<KNodeId256>(s, new FakeEndpointInvoker<KNodeId256>());
 
-        [TestMethod]
-        public async Task Can_randomly_populate_int160_mt()
-        {
-            var s = new KNodeId160(0, 0, 0);
-            var t = new KFixedTableRouter<KNodeId160>(s, new FakeEndpointInvoker<KNodeId160>());
+//            var r = new Random();
+//            var l = new List<Task>();
+//            for (int i = 0; i < 1024; i++)
+//                l.Add(t.UpdatePeerAsync(new KNodeId256((ulong)r.NextInt64(), (ulong)r.NextInt64(), (ulong)r.NextInt64(), (ulong)r.NextInt64()), null).AsTask());
 
-            var r = new Random();
-            var l = new List<Task>();
-            for (int i = 0; i < 1024; i++)
-                l.Add(t.UpdatePeerAsync(new KNodeId160((ulong)r.NextInt64(), (ulong)r.NextInt64(), (uint)r.Next(int.MinValue, int.MaxValue)), null).AsTask());
+//            await Task.WhenAll(l);
+//        }
 
-            await Task.WhenAll(l);
-        }
+//        [TestMethod]
+//        public async Task Should_find_buckets()
+//        {
+//            var s = KNodeId<KNodeId32>.Create();
+//            var t = new KFixedTableRouter<KNodeId32>(s, new FakeEndpointInvoker<KNodeId32>());
 
-        [TestMethod]
-        public async Task Can_randomly_populate_int256()
-        {
-            var s = new KNodeId256(0, 0, 0, 0);
-            var t = new KFixedTableRouter<KNodeId256>(s, new FakeEndpointInvoker<KNodeId256>());
+//            var r = new Random();
+//            var l = new List<Task>();
+//            for (int i = 0; i < 1024; i++)
+//                await t.UpdatePeerAsync(KNodeId<KNodeId32>.Create(), null);
 
-            var r = new Random();
-            for (int i = 0; i < 262144 * 8; i++)
-                await t.UpdatePeerAsync(new KNodeId256((ulong)r.NextInt64(), (ulong)r.NextInt64(), (ulong)r.NextInt64(), (ulong)r.NextInt64()), null);
-        }
+//            var z = await t.SelectPeersAsync(KNodeId<KNodeId32>.Create(), 200);
+//            var o = z.ToList();
+//        }
 
-        [TestMethod]
-        public async Task Can_randomly_populate_int256_mt()
-        {
-            var s = new KNodeId256(0, 0, 0, 0);
-            var t = new KFixedTableRouter<KNodeId256>(s, new FakeEndpointInvoker<KNodeId256>());
+//    }
 
-            var r = new Random();
-            var l = new List<Task>();
-            for (int i = 0; i < 1024; i++)
-                l.Add(t.UpdatePeerAsync(new KNodeId256((ulong)r.NextInt64(), (ulong)r.NextInt64(), (ulong)r.NextInt64(), (ulong)r.NextInt64()), null).AsTask());
-
-            await Task.WhenAll(l);
-        }
-
-        [TestMethod]
-        public async Task Should_find_buckets()
-        {
-            var s = KNodeId<KNodeId32>.Create();
-            var t = new KFixedTableRouter<KNodeId32>(s, new FakeEndpointInvoker<KNodeId32>());
-
-            var r = new Random();
-            var l = new List<Task>();
-            for (int i = 0; i < 1024; i++)
-                await t.UpdatePeerAsync(KNodeId<KNodeId32>.Create(), null);
-
-            var z = await t.SelectPeersAsync(KNodeId<KNodeId32>.Create(), 200);
-            var o = z.ToList();
-        }
-
-    }
-
-}
+//}
